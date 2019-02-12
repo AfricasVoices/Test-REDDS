5f55984f47e84a1e33f31917727f7b0fd16e89d3
Isaack Mwenda <mwendaisaac@gmail.com>
2019-02-12 16:45:41 +0300

    Fixed identation, dev mode & survey coding plan list

--- project_test/lib/pipeline_configurations.py
+++ project_test/lib/pipeline_configurations.py
@@ -20,33 +20,33 @@ class CodingPlan(object):
         self.run_id_field = id_field
 
 class PipelineConfiguration(object):
-    
-    DEV_MODE = False
+   
+    DEV_MODE = True #Set to True when testing the pipeline - False removes data for AVF test contacts.
 
     TEST_SHOWS_CODING_PLAN = [
         CodingPlan(raw_field="internet_working_raw",
-        coded_field="internet_working_coded",
-        time_field="sent_on",
-        coda_filename="internet_working.json"
-        icr_filename="internet_working.csv",
-        run_id_field="internet_working_id",
-        analysis_file_key="internet_working_",
-        cleaner=None,
-        code_scheme=CodeSchemes.INTERNET),
+                coded_field="internet_working_coded",
+                time_field="sent_on",
+                coda_filename="internet_working.json"
+                icr_filename="internet_working.csv",
+                run_id_field="internet_working_id",
+                analysis_file_key="internet_working_",
+                cleaner=None,
+                code_scheme=CodeSchemes.INTERNET),
 
         CodingPlan(raw_field="water_filter_raw",
-        coded_field="water_filter_coded",
-        time_field="sent_on",
-        coda_filename="water_filter.json"
-        icr_filename="water_filter_icr.csv",
-        run_id_field="water_filter_id",
-        analysis_file_key="water_filter_",
-        cleaner=None,
-        code_scheme=CodeSchemes.WATER),
+                coded_field="water_filter_coded",
+                time_field="sent_on",
+                coda_filename="water_filter.json"
+                icr_filename="water_filter_icr.csv",
+                run_id_field="water_filter_id",
+                analysis_file_key="water_filter_",
+                cleaner=None,
+                code_scheme=CodeSchemes.WATER),
     ]
     
     @staticmethod
-    def test_clean_age(text):
+    def clean_age_within_range(text):
         age = swahili.DemographicCleaner.clean_age(text)
         if type(age) == int and 10 <= age < 100:
             return str(age)
@@ -55,34 +55,28 @@ class PipelineConfiguration(object):
 
     SURVEY_CODING_PLANS = [
         CodingPlan(raw_field="age_raw",
-        coded_field="age_coded",
-        time_field="age_time",
-        coda_filename="age.json",
-        analysis_file_key="age",
-        cleaner=lambda text: PipelineConfiguration.test_clean_age(text),
-        code_scheme=CodeScheme.AGE),
-    ]
+                coded_field="age_coded",
+                time_field="age_time",
+                coda_filename="age.json",
+                analysis_file_key="age",
+                cleaner=lambda text: PipelineConfiguration.test_clean_age(text),
+                code_scheme=CodeScheme.AGE),
 
-    SURVEY_CODING_PLANS.extend([
         CodingPlan(raw_field="gender_raw",
-        coded_field="gender_coded",
-        time_field="gender_time",
-        coda_filename="gender.json"
-        run_id_field="gender_id",
-        analysis_file_key="gender",
-        cleaner=swahili.DemographicCleaner.clean_gender,
-        code_scheme=CodeSchemes.GENDER)
-    ])
-        
-    LOCATION_CODING_PLANS = [
-        CodingPlan(raw_field="client_region_raw",
-        coded_field="client_region_coded",
-        time_field="client_region_time",
-        coda_filename="location.json"
-        run_id_field="client_region_id",
-        analysis_file_key="client_region_",
-        cleaner=None,
-        code_scheme=CodeSchemes.LOCATION),
-    ]
+                coded_field="gender_coded",
+                time_field="gender_time",
+                coda_filename="gender.json"
+                run_id_field="gender_id",
+                analysis_file_key="gender",
+                cleaner=swahili.DemographicCleaner.clean_gender,
+                code_scheme=CodeSchemes.GENDER),
 
-    SURVEY_CODING_PLANS.extend(LOCATION_CODING_PLANS)
+        CodingPlan(raw_field="client_region_raw",
+                coded_field="client_region_coded",
+                time_field="client_region_time",
+                coda_filename="location.json"
+                run_id_field="client_region_id",
+                analysis_file_key="client_region_",
+                cleaner=None,
+                code_scheme=CodeSchemes.LOCATION)
+    ]
--- project_test/lib/test_schemes.py
+++ project_test/lib/test_schemes.py
@@ -3,7 +3,7 @@ import json
 from core_data_modules.data_models import Scheme
 
 def _open_scheme(filename):
-    with open(f"code_schemes/{filename}","r") as f:
+    with open(f"code_schemes/{filename}", "r") as f:
         firebase_map = json.load(f)
         return Scheme.from_firebase_map(firebase_map)
 
