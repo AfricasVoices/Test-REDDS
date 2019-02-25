caad5713120b7370fe8428ea9b94d30a5e8cab83
Isaack Mwenda <mwendaisaac@gmail.com>
2019-02-25 12:05:31 +0300

    Add stage auto code surveys

---
+++ project_test/auto_code_surveys.py
@@ -0,0 +1,48 @@
+import time
+from os import path
+
+from core_data_modules.cleaners import swahili, Codes 
+from core_data_modules.cleaners.cleaning_utils import CleaningUtils 
+from core_data_modules.traced_data import Metadata
+from core_data_modules.traced_data.io import TracedDataCodaV2IO
+from core_data_modules.util import IOUtils
+
+from project_test.lib.pipeline_configurations import PipelineConfiguration
+from project_test.lib.test_schemes import CodeSchemes
+
+class AutoCodeSurveys(object):
+    SENT_ON_KEY = "sent_on"
+
+    @classmethod
+    def auto_code_surveys(cls, user, data, phone_uuid_table, coda_output_dir):
+        # Label missing data
+        for td in data:
+            missing_dict = dict()
+            for plan in PipelineConfiguration.SURVEY_CODING_PLANS:
+                if td.get(plan.raw_field, "") =="":
+                    na_label = CleaningUtils.make_label_from_cleaner_code(
+                        plan.code_scheme, plan.code_scheme.get_code_with_control_code(Codes.TRUE_MISSING),
+                        Metadata.get_call_location()
+                    )
+                    missing_dict[plan.coded_field] = na_label.to_dict()
+            td.append_data(missing_dict, Metadata(user, Metadata.get_call_location(), time.time()))
+
+        # Auto-code remaining data
+        for plan in PipelineConfiguration.SURVEY_CODING_PLANS:
+            if plan.cleaner is not None:
+                CleaningUtils.apply_cleaner_to_traced_data_iterable(user, data, plan.raw_field, plan.coded_field,
+                                                                    plan.cleaner, plan.code_scheme)
+                                                                    
+        # Output survey answers to coda for manual verification + coding
+        IOUtils.ensure_dirs_exist(coda_output_dir)
+        for plan in PipelineConfiguration.SURVEY_CODING_PLANS:
+            
+            TracedDataCodaV2IO.compute_message_ids(user, data, plan.raw_field, plan.id_field)
+
+            coda_output_path = path.join(coda_output_dir, plan.coda_filename)
+            with open(coda_output_path, 'w') as f:
+                TracedDataCodaV2IO.export_traced_data_iterable_to_coda_2(
+                    data, plan.raw_field, plan.time_field, plan.id_field, {plan.coded_field: plan.code_scheme}, f
+                )
+        print("Coda Survey files successfully exported")   
+        return data 
